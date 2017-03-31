72b4edc0f19b508f426aaef576117d054cea3ea2
Abdelmageed Sayed <abdelmageedsayed@hotmail.com>
2017-03-31 21:04:19 +0200

    test for GET getIt

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,15 +1,39 @@
 import React, {Component} from 'react';
 
+import {axiosInstance as axios, getIt} from '../endpoints';
+
 export default class App extends Component{
   
   constructor(props){
     super(props);
+    this.state = {
+      text: ''
+    }
+    this.getIt = this.getIt.bind(this);
+  }
+  
+  getIt(){
+    console.log('click');
+    axios.get(getIt)
+      .then((response)=> {
+      console.log(response.data);
+      this.setState({
+        text: response.data
+      });
+    })
+    .catch((error)=> {
+        if(error.response)
+          this.setState({
+            text: error.response
+          });
+      });
   }
   
   render(){
     return (
       <div>
-        Hello World
+        <div>{this.state.text}</div>
+        <button onClick={this.getIt}>Get it</button>
       </div>
     )
   }
