acb4264e78a6d0742ca8c1d5b455df71dccf9f95
Abdelmageed Sayed <abdelmageedsayed@hotmail.com>
2017-04-02 12:12:24 +0200

    Added reference to environment variable $DATA_URL

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
-//const DOMAIN = (process.env.NODE_ENV == 'production')? ''
 const config = {
   PORT: process.env.PORT || 3007,
-  DATA_URL: "mongodb://localhost:27017/voter"
+  DATA_URL: process.env.DATA_URL ||   "mongodb://localhost:27017/cross-trial"
 };
 
 export default config;
