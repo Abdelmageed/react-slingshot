e947323665af379cace3f370c8bf1b18b46ef69c
Abdelmageed Sayed <abdelmageedsayed@hotmail.com>
2017-04-05 15:31:38 +0200

    Removed dropping the collection and closing the db connection on reset. Passing done() to seed and reset as a finish callback.

--- util/seedData.js
+++ util/seedData.js
@@ -3,11 +3,13 @@ import mongoose from 'mongoose';
 import config from '../config';
 //import User from '../src/server/models/User';
 
-mongoose.connect(config.DATA_URL);
-console.log('connected to db at seeding script');
+if(!mongoose.connection._hasOpened) {
+  mongoose.connect(config.DATA_URL);
+  console.log('connected to db at seeding script');
+}
+const User = mongoose.model('User');
 
-export function seed() {
-  const User = mongoose.model('User');
+export function seed(done) {
   let user = new User({
     local: {
       username: "Abdelmageed",
@@ -17,14 +19,15 @@ export function seed() {
   user.save((err, savedUser)=> {
     if (err) throw err;
     console.log(`user ${savedUser} saved`);
+    done();
   });
 }
 
-export function reset() {
-  User.remove({});
-  delete mongoose.models.User;
-  delete mongoose.modelSchema.User:
-  mongoose.disconnect(()=> {
-    console.log('db reset and connection closed');
+export function reset(done) {
+
+  User.remove({}, (err)=> {
+    if (err) throw err;
+    console.log('user collection reset');
+    done();
   });
-}
+  }
